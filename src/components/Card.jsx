3ad1202e08b159eb1c99f7bbd3123c3f6dbb50ad
AniketSinha <aniketkishore100@gmail.com>
2024-05-15 20:59:20 +0530

    feat: add final UI touches

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,43 +1,54 @@
 import { useState } from 'react';
 import { isNonEmptyString } from '../helpers';
 
+
 function Card({ jobData }) {
   return (
     <>
       <div className='card'>
-        <div className='chip'>
-          <div className='icon' />
-          <p>Posted 10 days ago</p>
-        </div>
-        <div className='flex marginTop10'>
-          <img src={jobData?.logoUrl} style={{ width: '60px', height: '60px', marginRight: '20px' }} />
-          <div className=''>
-            <p>{jobData?.companyName}</p>
-            <p>{jobData?.jobRole}</p>
-            <p>{jobData?.location}</p>
+        <div style={{ flex: 1 }}>
+          <div className='chip'>
+            <img src={'/hourglass.png'} style={{ width: '12px', height: '12px', marginRight: '4px' }} />
+            <p style={{ fontSize: '12px' }}>Posted 10 days ago</p>
+          </div>
+          <div className='flex marginTop10'>
+            <img src={jobData?.logoUrl} style={{ width: '60px', height: '60px', marginRight: '20px' }} />
+            <div className=''>
+              <p style={{ fontSize: '14px', fontWeight: 600, color: 'GrayText' }}>{jobData?.companyName}</p>
+              <p style={{ fontSize: '16px', fontWeight: 400 }}>{jobData?.jobRole}</p>
+              <p style={{ fontSize: '13px', fontWeight: 500 }}>{jobData?.location}</p>
+            </div>
+          </div>
+          <div className='marginTop10'>
+            <p style={{ fontSize: '14px', fontWeight: 600, color: 'GrayText' }}>Estimiated Salary: {jobData?.minJdSalary && `${jobData?.minJdSalary} - `}{jobData?.maxJdSalary}</p>
+          </div>
+          <div className='marginTop10'>
+            <p style={{ fontSize: '16px', fontWeight: 500 }}>About Company</p>
+            <p style={{ fontWeight: 600, fontSize: '14px' }}>About Us</p>
+            <p style={{ fontWeight: 400, fontSize: '14px' }}>{jobData?.jobDetailsFromCompany}</p>
+          </div>
+
+
+          <div className='marginTop10'>
+            {jobData?.minExp ?
+              <>
+                <p style={{ fontSize: '14px', fontWeight: 600, color: 'GrayText' }}>Minimum Experience</p><p style={{ fontSize: '16px', fontWeight: 500 }}>
+                  {jobData?.minExp} years
+                </p>
+              </>
+              : <div />
+            }
           </div>
         </div>
-        <div className='marginTop10'>
-          <p>Estimiated Salary: ${jobData?.minJdSalary && `${jobData?.minJdSalary} - `}{jobData?.maxJdSalary}</p>
-        </div>
-        <div className='marginTop10'>
-          <p>About Company</p>
-          <p style={{ fontWeight: 600, fontSize: 'small' }}>About Us</p>
-          <p>{jobData?.jobDetailsFromCompany}</p>
-        </div>
 
-        <div className='marginTop10'>
-          <p>Minimum Experience</p>
-          <p>
-            {jobData?.minExp} years
-          </p>
+        <div>
+          <a href={jobData?.jdLink} className='btn easyApplyBtn flex'>
+            <img src={'/lightening.png'} style={{ width: '16px', height: '16px', marginRight: '4px' }} />Easy Apply
+          </a>
+          <a href={jobData?.jdLink} className='btn applyReferralBtn '>
+            Unlock referral asks
+          </a>
         </div>
-        <button onClick={() => console.log('Easy Apply')} className='btn easyApplyBtn'>
-          Easy Apply
-        </button>
-        <button onClick={() => console.log('Easy Apply')} className='btn applyReferralBtn'>
-          Unlock referral asks
-        </button>
       </div>
     </>
   );
